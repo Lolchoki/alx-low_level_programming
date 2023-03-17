4ef100d2df7684d961ea86927139d6b007ce24bd
Lolchoki <lolchokijack@gmail.com>
2023-03-17 06:09:33 -0700

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include (stdio.h)
+
+/**
+ * main - this is the main fucntion
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+       put("\"Programming is like building a multilingual puzzle);
+       return (0); 
+}
+

a4ea6ea13aa6478a84acb8c5c9c046ef9e4b579e
Lolchoki <lolchokijack@gmail.com>
2023-03-29 03:06:36 -0700

    concatenation

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ *Return: on success 1
+ *on error, -1 is returned, and errno is set approprietly
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+ 

bb0e52741ca49075d6ce664748e2760e4a4b072b
Lolchoki <lolchokijack@gmail.com>
2023-03-30 04:20:49 -0700

    task 3 compare 2 strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,10 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *leet(char *);
+int _strcmp(char *s1, char *s2);
+
+
+
+
 
 #endif
